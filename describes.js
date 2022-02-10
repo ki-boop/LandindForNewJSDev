0f9adb55812dc0081ec7381aafac6924a080f40c
ki-boop <karpakovk_ok@mail.ru>
2022-02-11 02:19:44 +0400

    OK

--- describes.js
+++ describes.js
@@ -1,22 +1,63 @@
+var idtask;
+var txt;
+var speed=200;
+var i =0;
+var count =0;
+var timeout;
+
+function typeWriter (obj) {
+    if (i<txt.length){
+        console.log(i);
+        idtask.childNodes[0].innerText+=txt.charAt(i);
+        i++;
+    }
+    timeout =  setTimeout(typeWriter, speed);
+    
+}
 let idtask0 = document.getElementById('task0')
 let des0 = document.getElementById('des0');
 
 idtask0.addEventListener("mouseleave", (event)=>{
+    idtask0.childNodes[0].innerText = "0";
+    count=0;
+    clearTimeout(timeout);
+    i=0;
     des0.style.display = 'none';
 })
 
 idtask0.addEventListener("mouseover", (event)=>{
+    
+    if(count == 0){
+        txt="Приветствие";
+        idtask=idtask0;
+        idtask0.childNodes[0].innerText = "";
+        typeWriter();
+        count=1;
+    }
     des0.style.display = 'block';
+
 })
 
 let idtask1 = document.getElementById('task1')
 let des1 = document.getElementById('des1');
 
 idtask1.addEventListener("mouseleave", (event)=>{
+    idtask1.childNodes[0].innerText = "1";
+    count=0;
+    clearTimeout(timeout);
+    i=0;
     des1.style.display = 'none';
     
 })
 idtask1.addEventListener("mouseover", (event)=>{
+    
+    if(count == 0){
+        txt="Площадь";
+        idtask=idtask1;
+        idtask1.childNodes[0].innerText = "";
+        typeWriter();
+        count=1;
+    }
     des1.style.display = 'block';
 })
 
@@ -25,10 +66,21 @@ let idtask2 = document.getElementById('task2')
 let des2 = document.getElementById('des2');
 
 idtask2.addEventListener("mouseleave", (event)=>{
+    idtask2.childNodes[0].innerText = "2";
+    count=0;
+    clearTimeout(timeout);
+    i=0;
     des2.style.display = 'none';
     
 })
 idtask2.addEventListener("mouseover", (event)=>{
+    if(count == 0){
+        txt="Сравнение";
+        idtask=idtask2;
+        idtask2.childNodes[0].innerText = "";
+        typeWriter();
+        count=1;
+    }
     des2.style.display = 'block';
 })
 
@@ -36,10 +88,21 @@ let idtask3 = document.getElementById('task3')
 let des3 = document.getElementById('des3');
 
 idtask3.addEventListener("mouseleave", (event)=>{
+    idtask3.childNodes[0].innerText = "3";
+    count=0;
+    clearTimeout(timeout);
+    i=0;
     des3.style.display = 'none';
     
 })
 idtask3.addEventListener("mouseover", (event)=>{
+    if(count == 0){
+        txt="Max&Min";
+        idtask=idtask3;
+        idtask3.childNodes[0].innerText = "";
+        typeWriter();
+        count=1;
+    }
     des3.style.display = 'block';
 })
 
@@ -47,10 +110,21 @@ let idtask4 = document.getElementById('task4')
 let des4 = document.getElementById('des4');
 
 idtask4.addEventListener("mouseleave", (event)=>{
+    idtask4.childNodes[0].innerText = "4";
+    count=0;
+    clearTimeout(timeout);
+    i=0;
     des4.style.display = 'none';
     
 })
 idtask4.addEventListener("mouseover", (event)=>{
+    if(count == 0){
+        txt="Таймер";
+        idtask=idtask4;
+        idtask4.childNodes[0].innerText = "";
+        typeWriter();
+        count=1;
+    }
     des4.style.display = 'block';
 })
 
@@ -58,10 +132,21 @@ let idtask5 = document.getElementById('task5')
 let des5 = document.getElementById('des5');
 
 idtask5.addEventListener("mouseleave", (event)=>{
+    idtask5.childNodes[0].innerText = "5";
+    count=0;
+    clearTimeout(timeout);
+    i=0;
     des5.style.display = 'none';
     
 })
 idtask5.addEventListener("mouseover", (event)=>{
+    if(count == 0){
+        txt="ТестJS";
+        idtask=idtask5;
+        idtask5.childNodes[0].innerText = "";
+        typeWriter();
+        count=1;
+    }
     des5.style.display = 'block';
 })
 
@@ -69,9 +154,20 @@ let idtask6 = document.getElementById('task6')
 let des6 = document.getElementById('des6');
 
 idtask6.addEventListener("mouseleave", (event)=>{
+    idtask6.childNodes[0].innerText = "6";
+    count=0;
+    clearTimeout(timeout);
+    i=0;
     des6.style.display = 'none';
     
 })
 idtask6.addEventListener("mouseover", (event)=>{
+    if(count == 0){
+        txt="Анимация";
+        idtask=idtask6;
+        idtask6.childNodes[0].innerText = "";
+        typeWriter();
+        count=1;
+    }
     des6.style.display = 'block';
 })
--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let personeName = document.getElementById('person');
 
+var score = 0;
+
 
 
 const task0 = ()=>{
@@ -75,6 +77,80 @@ const task4Stop = () =>{
     pause=true;
     clearInterval(inter);
     return;
+}
+
+const task5 = () =>{
+    let test1 = document.getElementById('second-floor');
+    test1.style.display = 'flex';
+    let test2 = document.getElementById('first-floor');
+    test2.style.display = 'flex';
+
+}
+
+const testButt=(numTask)=>{
+    let block = document.getElementById('scoreTest');
+    block.style.display = 'block';
+    let answer= document.getElementById('a'+numTask);
+    let curr = document.getAnimations('a');
+    console.log(numTask)
+    if(answer.checked && answer.value==0) {
+        score++;
+        answer.parentNode.parentNode.childNodes[0].style.backgroundColor = 'green';
+        answer.value = 1;
+    }
+    if(!answer.checked && answer.value==1){
+        if (answer.parentNode.parentNode.childNodes[0].style.backgroundColor == 'green') score--;
+        answer.parentNode.parentNode.childNodes[0].style.backgroundColor = 'red';
+        answer.value=0;
+    }
+    if (!answer.checked){
+        answer.parentNode.parentNode.childNodes[0].style.backgroundColor = 'red';
+    }
+    block.innerText = score +'/10';
+}
+
+
+
+const enterName = () =>{
+    
+}
+var ii=0;k=100;
+const task6 = () =>{
+
+    
+    let img = document.getElementById('animate');
+    img.style.display='block';
+    let place = document.getElementById('placeName')
+    let placeDate = document.getElementById('date')
+    let name = sessionStorage.getItem ('Name');
+    placeDate.innerText = new Date().getDate()+"." + new Date().getMonth()+"."+new Date().getFullYear();
+    place.innerText=name;
+    img.addEventListener("click", (event)=>{
+        console.log(k)
+        console.log(ii);
+        
+        const timerI = setInterval(() => {
+            ii=80;
+            k++;
+            ii=100-k;
+            img.style.opacity = ii/100 ;
+            if(ii <= 0||k>=100) clearInterval(timerI);
+            if (k==100) img.style.display='none';
+        }, 20);
+       
+    })
+    
+       
+        const timerI = setInterval(() => {
+        k--;
+        ii=100-k;
+        img.style.opacity = ii/100 ;
+        if(k <= 0||ii>=100) clearInterval(timerI);
+    }, 20);
+
+    k=100;
+    ii=0;
+
 }
     
 
